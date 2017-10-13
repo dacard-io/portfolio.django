325cc98e05c63348afef89e3ef1c624814c9205e
David Cardoso <davidcardoso93@gmail.com>
2017-10-13 01:39:25 -0400

    Minor bug fix

--- app/static/assets/js/main.js
+++ app/static/assets/js/main.js
@@ -153,7 +153,6 @@ project_container.packery(isotope_properties);
 
 // Push menu button action
 $('.push-menu-button, #close-push-menu').on("mousedown", function(){
-  console.log("Side menu opened")
   $('.site-container').toggleClass('menu-open');
   $('.push-menu').toggleClass('open'); 
   $('body').toggleClass('no-v-scroll'); 
