d0694ea61d5c7a51ef6e001485ab1dbc7239a6f5
David Cardoso <davidcardoso93@gmail.com>
2017-10-26 01:48:25 -0400

    Removed unused dependency

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from mce_filebrowser.admin import MCEFilebrowserAdmin
 
 # So this is where you add models so you can use them in Django's admin panel
 from .models import Post
